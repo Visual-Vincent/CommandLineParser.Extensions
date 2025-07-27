28ce17c93c7a2ebfa0e536e1878c4bebaf74f6e3
Visual-Vincent <github-vv@mydoomsite.com>
2025-07-27 19:38:44 +0200

    Initial commit

---
+++ CommandLineParser.Extensions/Command.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandLine.Text;
+
+namespace CommandLine.Extensions
+{
+    /// <summary>
+    /// The base class for a program command.
+    /// </summary>
+    public abstract class Command
+    {
+        private static readonly Type[] Commands = Assembly.GetEntryAssembly()
+            .GetTypes()
+            .Where(t => !t.IsNested && t.BaseType == typeof(Command) && t.GetCustomAttribute<VerbAttribute>(false) != null)
+            .ToArray();
+
+        private HelpText helpText;
+
+        /// <summary>
+        /// Gets the default help text for this command.
+        /// </summary>
+        protected HelpText HelpText
+        {
+            get {
+                if(helpText == null)
+                {
+                    if(ParseResult.Tag == ParserResultType.Parsed)
+                    {
+                        // Build the help text for a command that was successfully parsed
+                        helpText = HelpText.AutoBuild(ParseResult, text => text, text => text, true);
+
+                        var nestedCommands = this.GetType()
+                            .GetNestedCommands()
+                            .ToArray();
+
+                        if(nestedCommands.Length > 0)
+                            helpText.AddVerbs(nestedCommands);
+                    }
+                    else
+                    {
+                        helpText = HelpText.AutoBuild(ParseResult);
+                    }
+                }
+
+                return helpText;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ParserResult{T}"/> generated from parsing this command.
+        /// </summary>
+        protected ParserResult<object> ParseResult { get; private set; }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        public abstract void Execute();
+
+        /// <summary>
+        /// Parses the specified arguments using the default parser and executes the respective command, if one is found.
+        /// </summary>
+        /// <param name="args">The arguments sent to the application.</param>
+        /// <returns>A <see cref="ParserResult{T}"/> containing the appropriate instance with parsed values as a <see cref="object"/> and a sequence of <see cref="Error"/>.</returns>
+        public static ParserResult<object> ParseAndExecute(string[] args)
+        {
+            return ParseAndExecute(Parser.Default, args);
+        }
+
+        /// <summary>
+        /// Parses the specified arguments and executes the respective command, if one is found.
+        /// </summary>
+        /// <param name="parser">The parser to use to parse the command line arguments.</param>
+        /// <param name="args">The arguments sent to the application.</param>
+        /// <returns>A <see cref="ParserResult{T}"/> containing the appropriate instance with parsed values as a <see cref="object"/> and a sequence of <see cref="Error"/>.</returns>
+        public static ParserResult<object> ParseAndExecute(Parser parser, string[] args)
+        {
+            var result = parser.ParseCommands(args, Commands);
+
+            result.WithParsed(obj => {
+                if(!(obj is Command cmd))
+                    throw new InvalidOperationException($"Parsed command (of type {obj.GetType().FullName}) does not inherit from {typeof(Command).FullName}");
+
+                cmd.ParseResult = result;
+                cmd.Execute();
+            });
+
+            return result;
+        }
+    }
+}
---
+++ CommandLineParser.Extensions/CommandLineExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandLine.Extensions
+{
+    /// <summary>
+    /// Static class containing extension methods for managing the command line.
+    /// </summary>
+    public static class CommandLineExtensions
+    {
+        /// <summary>
+        /// Searches for nested types, that derive from <see cref="Command"/> and have the <see cref="VerbAttribute"/> applied, in the current <see cref="Type"/>.
+        /// </summary>
+        /// <param name="type">The type to search in.</param>
+        /// <returns>The collection of nested <see cref="Command"/> types.</returns>
+        internal static IEnumerable<Type> GetNestedCommands(this Type type)
+        {
+            if(type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            return type
+                .GetNestedTypes(BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(t => typeof(Command).IsAssignableFrom(t) && t.GetCustomAttribute<VerbAttribute>(false) != null);
+        }
+
+        /// <summary>
+        /// Parses a string array of command line arguments for verb commands scenario. This differs from ParseArguments in that this has support for nested commands.
+        /// The <see cref="VerbAttribute"/> must be applied to types in the array and each type must inherit from <see cref="Command"/>.
+        /// </summary>
+        /// <param name="parser">The parser to use.</param>
+        /// <param name="args">A <see cref="string"/> array of command line arguments, normally supplied by application entry point.</param>
+        /// <param name="types">A <see cref="Type"/> array used to supply command alternatives. Should contain only the top-level commands. Nested commands are found automatically.</param>
+        /// <returns>A <see cref="ParserResult{T}"/> containing the appropriate instance with parsed values as a <see cref="object"/>
+        /// and a sequence of <see cref="Error"/>.</returns>
+        /// <returns>A <see cref="ParserResult{T}"/> containing the appropriate instance with parsed values as a <see cref="object"/> and a sequence of <see cref="Error"/>.</returns>
+        /// <remarks>All types must expose a parameterless constructor and inherit from <see cref="Command"/>.</remarks>
+        public static ParserResult<object> ParseCommands(this Parser parser, string[] args, params Type[] types)
+        {
+            if(parser == null)
+                throw new ArgumentNullException(nameof(parser));
+
+            if(args == null)
+                throw new ArgumentNullException(nameof(args));
+
+            if(types == null)
+                throw new ArgumentNullException(nameof(types));
+
+            var verb = args.FirstOrDefault();
+
+            // If the argument starts with "-" it is not a verb, but possibly a flag
+            if(verb?.StartsWith("-") ?? false)
+                return parser.ParseArguments(args, types);
+
+            var stringComparison = parser.Settings.CaseSensitive
+                ? StringComparison.Ordinal
+                : StringComparison.OrdinalIgnoreCase;
+
+            foreach(var type in types)
+            {
+                if(!typeof(Command).IsAssignableFrom(type))
+                    continue;
+
+                var verbAttribute = type.GetCustomAttribute<VerbAttribute>(false);
+
+                if(verbAttribute == null || !verbAttribute.Name.Equals(verb, stringComparison))
+                    continue;
+
+                var nested = type
+                    .GetNestedCommands()
+                    .ToArray();
+
+                if(nested.Length <= 0 || args.Length <= 1)
+                    break;
+
+                return ParseCommands(parser, args.Skip(1).ToArray(), nested);
+            }
+
+            return parser.ParseArguments(args, types);
+        }
+    }
+}
